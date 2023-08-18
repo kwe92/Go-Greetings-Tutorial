bf0f2db82840096fe57c21ba855e38441a1f5bc8
Kwe <Kweayon@gmail.com>
2023-08-18 14:10:51 -0500

    update: initial commit

---
+++ greetings/greetings.go
@@ -0,0 +1,55 @@
+package greetings
+
+import (
+	"errors"
+	"fmt"
+)
+
+// Hello returns a greeting of the named person.
+func Hello(name string) (string, error) {
+	// if the name is empty throw an error
+	if name == "" {
+		return "", errors.New("Name can not be empty, provide a name.")
+	}
+	message := fmt.Sprintf("\nHello %v, welcome to the Go community!\n\n", name)
+
+	return message, nil
+}
+
+// fmt.Sprintf
+
+//   ~ a format string, similar to python format string literals:
+//       + f string literal or more precisely literal string dot format:
+// 		     - 'Hello %v, welcome to the Python community.format(message)'
+
+// errors package
+
+//   - a package that is part of the standard library
+//   - used to handle and throw errors
+
+// errors.New
+
+//   - returns a new error with the passed in error message argument
+
+// error handling with nil
+
+//   - when error handling in Go you return nil for the error type if there is no error
+
+// Functions Returning Multiple Values
+
+//   - In Go functions can return multiple values instead of needing to use a tuple / Record
+//   - the caller unpacks (Python) / destructures (TypeScript / Dart) the values
+//     into variables on the recieving end
+
+// multi return statements
+
+//   - every return statement used must return the number of variables
+//     specified in the return type head
+
+// Return data-type head
+
+//   - in Go you can ha ve two heads
+//   - the first head specifies the set of parameters and their types
+//   - the second head can be ommited if there is only 1 returned variable
+//   - if there is more than 1 variable being returned a second head is required
+//   - providing variable names within the return type head is optional (why add more optional code? K.I.S.M)
---
+++ hello/hello.go
@@ -0,0 +1,42 @@
+package main
+
+// TODO: ad comments from example
+
+import (
+	"fmt"
+	"log"
+
+	"example.com/greetings"
+)
+
+// Logger encapsulates logging.
+type Logger struct {
+}
+
+// Error
+func (logger Logger) Error(err error, logger_prefix string) {
+
+	// Sets the output prefix displayed to the console from the logger.
+	log.SetPrefix(logger_prefix)
+
+	// Sets the output flags for the logger.
+	log.SetFlags(0)
+
+	// Prints the error concatenating the logger prefix and exits the application with os.Exit(1)
+	log.Fatal(err)
+}
+
+func main() {
+	logger := Logger{}
+	message, err := greetings.Hello("")
+	if err != nil {
+		logger.Error(err, "greetings: ")
+	}
+	fmt.Printf(message)
+}
+
+// Executing Go code as an Application
+
+//   - declaring a package main lets the Go compiler know its an executable application
+//   - the declared main package does not have to be called main.go
+//   - there can only be one main package per module
