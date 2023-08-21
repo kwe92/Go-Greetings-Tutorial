81f44df5e936ebc4ff18ef237342e6a6f6ea7d97
Kwe <Kweayon@gmail.com>
2023-08-21 09:44:47 -0500

    update: added IsEmpty and HasError implementations

--- utility/utility.go
+++ utility/utility.go
@@ -4,6 +4,24 @@ import (
 	"math/rand"
 )
 
+// RandInt returns an integer from [0, exclusive_range)
 func RandInt(exclusive_range int) (random_integer int) {
 	return rand.Intn(exclusive_range)
+}
+
+// IsEmpty is a predicate checking if a string is empty.
+func IsEmpty(str string) bool {
+	if str == "" {
+		return true
+	}
+	return false
+
+}
+
+// HasError is a predicate checking if an error was returned.
+func HasError(err error) bool {
+	if err != nil {
+		return true
+	}
+	return false
 }
